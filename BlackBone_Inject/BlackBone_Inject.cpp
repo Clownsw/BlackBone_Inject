eea8fcec53aa2e4d41e6b5dce211d4d4944908a4
Smliexx <3059926098@qq.com>
2021-02-09 05:24:00 +0000

    Update BlackBone_Inject.cpp

--- BlackBone_Inject/BlackBone_Inject.cpp
+++ BlackBone_Inject/BlackBone_Inject.cpp
@@ -33,7 +33,6 @@ const std::wstring initArg = L"";
 
 TCHAR ProcessWindowName[MAX_PATH + 1];
 TCHAR szValue[MAX_PATH + 1];
-int unlink_ = 0, erasePE_ = 0;
 
 Func_Initialize Initialize;
 Func_InjectDll InjectDll;
@@ -42,27 +41,6 @@ void Init(HMODULE _hModule)
 {
 	Initialize = (Func_Initialize)GetProcAddress(_hModule, reinterpret_cast<LPCSTR>(1));
 	InjectDll = (Func_InjectDll)GetProcAddress(_hModule, reinterpret_cast<LPCSTR>(13));
-}
-
-void inject(void)
-{
-	if (unlink_ == 1 && erasePE_ == 1)
-	{
-		InjectDll(pid, szValue, IT_Thread, 0, initArg, true, true, true);
-	}
-	else if (unlink_ == 0 && erasePE_ == 1)
-	{
-		InjectDll(pid, szValue, IT_Thread, 0, initArg, false, true, true);
-	}
-	else if (unlink_ == 1 && erasePE_ == 0)
-	{
-		InjectDll(pid, szValue, IT_Thread, 0, initArg, true, false, true);
-	}
-	else if (unlink_ == 0 && erasePE_ == 0)
-	{
-		InjectDll(pid, szValue, IT_Thread, 0, initArg, false, false, true);
-	}
-	
 }
 
 int main()
@@ -78,9 +56,6 @@ int main()
 	std::cout << "DriverHandle: " << DriverHandle << std::endl;
 
 	std::cout << InjectDll << std::endl;
-	
-	unlink_ = GetPrivateProfileInt(L"z.smliexx.ml", L"unlink", 0, L"./Inject.ini");
-	erasePE_ = GetPrivateProfileInt(L"z.smliexx.ml", L"erasePE", 0, L"./Inject.ini");
 
 	GetPrivateProfileString(L"z.smliexx.ml", L"ProcessWindowName", L"-1", ProcessWindowName, MAX_PATH, L"./Inject.ini");
 	GetPrivateProfileString(L"z.smliexx.ml", L"DllPath", L"-1", szValue, MAX_PATH, L"./Inject.ini");
@@ -92,12 +67,9 @@ int main()
 	GetWindowThreadProcessId(Windowhwnd, &pid);
 
 	std::cout << "Pid: " << pid << std::endl;
-
-	std::cout << "unlink: " << unlink_ << std::endl;
-	std::cout << "erasePE: " << erasePE_ << std::endl;
-
-	inject();
-
+	
+	InjectDll(pid, szValue, IT_Thread, 0, initArg, false, false, true);
+	
 	if (DriverHandle != NULL)
 	{
 		CloseHandle(DriverHandle);
